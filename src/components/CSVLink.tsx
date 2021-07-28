ea58dd7c3f0a9507934a7b8d0d460b1dcfa20f69
zubairlalani <zubairlalani07@gmail.com>
2021-07-28 11:37:29 -0400

    Make csv download a button

--- src/components/CSVLink.tsx
+++ src/components/CSVLink.tsx
@@ -7,19 +7,42 @@ interface Props {
 }
 
 export class CSV extends React.Component<Props> {
+    private csvLink: any;
     constructor(props: Props) {
         super(props);
+        this.csvLink = React.createRef();
     }
 
     shouldComponentUpdate(nextProps: Props) {
         return this.props["data"] !== nextProps["data"];
     }
+    // fetchData = () => {
+    //     fetch('/mydata/'+this.props.id).then(data => {
+    //         console.log(data);
+    //       this.setState({ data:data }, () => {
+    //         // click the CSVLink component to trigger the CSV download
+    //         this.csvLink.current.link.click()
+    //       })
+    //     })
+    //   }
 
     render() {
         
         const separator = "\t"
         const enclosing_char = "'";
-        return <CSVLink data={this.props.data} separator={separator} filename={"ClusteredBins.txt"} style={{marginLeft: 100}}> Download to CSV</CSVLink>
+        let csvButton = <div>
+            
+            <CSVLink 
+                data={this.props.data} 
+                separator={separator}
+                filename="ClusteredBins.txt"
+                className="hidden"
+                ref={(r:any) => this.csvLink = r}
+                target="_blank" />
+            <button type="button" onClick={(e) => this.csvLink.link.click()}>Download</button>
+        </div>
+        return csvButton;
+        // return <CSVLink data={this.props.data} separator={separator} filename={"ClusteredBins.txt"} style={{marginLeft: 100}}> Download to CSV</CSVLink>
         //<CSVDownload data={this.props.data} filename={"ClusteredBins.txt"} separator={separator}></CSVDownload> //
     }
 }
